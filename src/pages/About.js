baf7012efc3bbcbf1f99e7cc4ed719e90440d57c
Shakyan <shakyankushwahaa@gmail.com>
2022-07-06 20:40:58 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+				Repudiandae, aspernatur commodi! Iusto enim corrupti molestiae
+				minus. Sapiente ab nam odit rem fugiat, doloremque, hic,
+				cupiditate eaque quisquam odio tempore neque? Adipisci nam ipsum
+				itaque dolorem eum sapiente laboriosam debitis praesentium!
+				Architecto expedita optio perspiciatis, repellendus velit
+				possimus quisquam itaque quo, cumque, ducimus quod dolores
+				doloribus qui vero est eaque ratione? Excepturi numquam vel
+				eveniet tempora quidem ipsam tempore hic
+			</p>
+		</section>
+	);
+};
+
+export default About;
